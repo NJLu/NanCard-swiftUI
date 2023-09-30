3e11a4d3e68f3fa22ef24e9b114dce32f0464c8d
Kei <nj.lu1226@gmail.com>
2023-09-30 12:58:14 -0400

    Initial Commit

---
+++ NanCard/NanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanCardApp.swift
+//  NanCard
+//
+//  Created by Nanjiong Lu on 2023-09-30.
+//
+
+import SwiftUI
+
+@main
+struct NanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
